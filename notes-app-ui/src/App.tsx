e8ef61c51580979044f409108c2c8a46c52e07fb
krith-raju <kraju30@gatech.edu>
2024-06-04 13:43:43 -0400

    Initial commit

---
+++ notes-app-ui/src/App.tsx
@@ -0,0 +1,67 @@
+
+import { useState } from "react";
+import "./App.css"
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "note title 1",
+      content: "content 1"
+    },
+    {
+      id: 2,
+      title: "note title 2",
+      content: "content 2"
+    },
+    {
+      id: 3,
+      title: "note title 3",
+      content: "content 3"
+    },
+    {
+      id: 4,
+      title: "note title 4",
+      content: "content 4"
+    },
+    {
+      id: 5,
+      title: "note title 5",
+      content: "content 5"
+    },
+
+  ])
+  return ( 
+  <div className = "app-container">
+    <form className= "note-form">
+      <input
+       placeholder="title"
+       required>
+        
+      </input>
+      <textarea
+      placeholder="Content"
+      rows={10}
+      required>
+        
+      </textarea>
+      <button type="submit">
+        Add Note
+      </button>
+    </form>
+    <div className="notes-grid">
+      {notes.map((note) =>  (
+        <div className="note-item">
+        <div className="notes-header">
+          <button> x </button>
+        </div>
+        <h2>{note.title}</h2>
+        <p> {note.content}</p>
+      </div>
+      ))};
+    </div>
+  </div>
+  );
+};
+
+export default App;
